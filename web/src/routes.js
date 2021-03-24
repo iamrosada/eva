f764786e24dcfaf1d4827a1a25c69c4d9ef82617
77-aguarosada <luisrosada2018@gmail.com>
2021-03-24 12:16:18 -0300

    added router

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,8 +1,15 @@
 
 import {Route,Switch} from 'react-router-dom'
+import AllCountry from './pages/AllCountry';
+import AllRooms from './pages/AllRooms';
 import AllStudent from './pages/AllStudent';
+import CreateCountry from './pages/CreateCountry';
+import CreateRooms from './pages/CreatedRooms';
 import CreateStudent from './pages/CreateSudent';
+import DeleterCountry from './pages/DeleterCountry';
+import DeleterRooms from './pages/DeleterRooms';
 import DeleteStudent from './pages/DeleteStudent';
+import EditerCountry from './pages/EditCountry';
 import EditStudent from './pages/EditStudent';
 import Home from './pages/Home';
 
@@ -22,8 +29,15 @@ function Routes(){
         <Route path="/editstudent" exact component={EditStudent} />
         <Route path="/allstudent" exact component={AllStudent} />
         <Route path="/deletstudent" exact component={DeleteStudent} />
+        <Route path="/createrooms" exact component={CreateRooms} />
+        
+        <Route path="/deleterooms" exact component={DeleterRooms} />
+        <Route path="/allrooms" exact component={AllRooms} />
 
-
+        <Route path="/allcountry" exact component={AllCountry} />
+        <Route path="/createcountry" exact component={CreateCountry} />
+        <Route path="/deletercountry" exact component={DeleterCountry} />
+        <Route path="/editercountry" exact component={EditerCountry} />
     </Switch>
         
 
