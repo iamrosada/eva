6153f896d70f704e2845eb19826fc53170714bf9
77-aguarosada <luisrosada2018@gmail.com>
2021-03-28 11:16:00 -0300

    added new router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -22,8 +22,9 @@ const studentController = new StudentController()
 
 router.post("/students",studentController.create)
 
-router.get("/students",studentController.show)
 
+router.get("/students",studentController.show)
+router.get("/students/:surname",studentController.showforSurname)
 router.put("/students/:id",studentController.update)
 
 router.delete("/students/:id",studentController.delete)
