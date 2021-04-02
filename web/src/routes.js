487a53ee74a417488a4aaac390305a4f5f06c39f
77-aguarosada <luisrosada2018@gmail.com>
2021-04-01 22:43:06 -0300

    deleted some router

--- web/src/routes.js
+++ web/src/routes.js
@@ -6,10 +6,10 @@ import AllStudent from './pages/AllStudent';
 import CreateCountry from './pages/CreateCountry';
 import CreateRooms from './pages/CreatedRooms';
 import CreateStudent from './pages/CreateSudent';
-import DeleterCountry from './pages/DeleterCountry';
-import DeleterRooms from './pages/DeleterRooms';
+
+
 import DeleteStudent from './pages/DeleteStudent';
-import EditerCountry from './pages/EditCountry';
+
 import EditStudent from './pages/EditStudent';
 import Home from './pages/Home';
 
@@ -31,13 +31,12 @@ function Routes(){
         <Route path="/deletstudent" exact component={DeleteStudent} />
         <Route path="/createrooms" exact component={CreateRooms} />
         
-        <Route path="/deleterooms" exact component={DeleterRooms} />
+      
         <Route path="/allrooms" exact component={AllRooms} />
 
         <Route path="/allcountry" exact component={AllCountry} />
         <Route path="/createcountry" exact component={CreateCountry} />
-        <Route path="/deletercountry" exact component={DeleterCountry} />
-        <Route path="/editercountry" exact component={EditerCountry} />
+      
     </Switch>
         
 
