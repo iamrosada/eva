59b08f1303baed29fe88f0185f6997d58d8ecfc3
77-aguarosada <luisrosada2018@gmail.com>
2021-04-01 22:42:47 -0300

    configurating toast for use in all app

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Routes from './routes'
-
+import {ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
         <BrowserRouter>
+        <ToastContainer autoClose={5000}/>
           <Routes/>
-        
         </BrowserRouter>
   );
 }
