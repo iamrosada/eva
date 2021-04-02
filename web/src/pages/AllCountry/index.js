5d21e22739b51fc4075d84a4c99bec7622796e61
77-aguarosada <luisrosada2018@gmail.com>
2021-04-01 22:44:00 -0300

    added usestate and useEffect

--- web/src/pages/AllCountry/index.js
+++ web/src/pages/AllCountry/index.js
@@ -1,6 +1,8 @@
 
-
+import React, { useState,   useEffect } from "react";
 import "./styles.css"
+
+
 import {FiChevronRight} from 'react-icons/fi'
 import {MdSchool} from 'react-icons/md'
 import {MdBusiness}from 'react-icons/md'
@@ -9,9 +11,35 @@ import {MdCreate} from 'react-icons/md'
 import {MdFormatAlignLeft}from 'react-icons/md'
 import {MdDelete}from 'react-icons/md'
 import {Link} from  'react-router-dom'
+import api from "../../services/api";
 
 export default function AllCountry(){
+  const [show, setShow] = useState(false)
+  const [allstudent,setAllStudent] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredStudenty, setFilteredStudenty] = useState([]);
+  const [ modalContant,setModalContant] = useState({}); 
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result =  await api.get("/country")
+ 
+     setAllStudent(result.data);
+     console.log(result.data)
+    };
+    
+    fetchData();
+  }, []);
 
+
+  
+  useEffect(() => {
+   setFilteredStudenty(
+     allstudent.filter((student) =>
+       student.countryStudent.toLowerCase().includes(search.toLowerCase())
+     )
+   );
+ }, [search, allstudent]);
 
   
 
@@ -41,7 +69,7 @@ export default function AllCountry(){
                         <div id="option-student-home-page">
                             {/*<span>todos estudantes</span>*/}
                             <ul>
-                            <Link className="select" to="/home">
+                            <Link className="sess" to="/home">
                                Recentes
                             </Link>
                               <Link to="/student">
@@ -63,49 +91,60 @@ export default function AllCountry(){
                                    <Link to="/createrooms">
                                    <IoIosAdd className="ic-left" color="#cbcbd6" size={25}/>Adicionar Quarto</Link>
 
-                                    <Link to="/deleterooms">
-                                    <MdDelete className="ic-left" color="#cbcbd6" size={25}/>Deletar Quarto </Link>
-
+                                   
                                      <Link to="/allrooms">
                                      <MdFormatAlignLeft className="ic-left" color="#cbcbd6" size={25}/>listar Quartos</Link>
 
                                    <Link to="/createcountry">
                                    <IoIosAdd className="ic-left" color="#cbcbd6" size={25}/>Criar País</Link>
 
-                                 <Link to="/editercountry">
-                                 <MdCreate className="ic-left"color="#cbcbd6" size={25}/>Editar País</Link>
+                                
 
                                <Link to="/allcountry">
                                <MdFormatAlignLeft className="ic-left" color="#cbcbd6" size={25}/>Listar País</Link>
-
-                            <Link to="/deletercountry">
-                            <MdDelete  className="ic-left" color="#cbcbd6" size={25}/>Deletar País</Link>
 
                             
                             </ul>
                         </div>
                         <div id="for-search">
                           <form id="form-input-home-page"action="">
-                               <input placeholder="search students"/>
+                          <input 
+                               placeholder="search students"
+                               onChange={(e) => setSearch(e.target.value)}
+                               />
                                <button type="submit">Search</button>
 
                           </form>
 
 
-                         
-                          
-                          <div id="students">
+                         {
+                            filteredStudenty.map(item=>(
+                            <div 
+                            id="students"
+                            key={item.id}
+
+                            onClick={() =>{
+                              setShow(true);
+                              
+                               setModalContant(item);
+                             }}
+                            >
                               <span id="countryy-studenty">COUNTRY</span>
-                              <span className="sp">ANGOLA</span>
-                              <span className="sp">Yolanda Barrueco</span>
-                              <div id="univer-home">
+                              <span className="sp">{item.countryStudent}</span>
+                            
+                              {/**<div id="univer-home">
                                 <MdSchool color="#cbcbd6"size={20}/>
                                 <span className="sp">UITS</span>
                                
-                              </div>
+                              </div> */}
                               <FiChevronRight color="#cbcbd6"size={20}/>
 
                           </div>
+
+                            ))
+                         }
+                          
+                          
                          
 
 
