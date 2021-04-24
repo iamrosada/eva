28afb218ea3a9d00c1d6e5b43ab694f2a4c9d26f
77-aguarosada <luisrosada2018@gmail.com>
2021-04-24 18:14:03 -0300

    added middlewares in routes student for uploads images

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,26 +1,30 @@
 
-import {Router} from 'express';
+import { Router, Request, Response, RouterOptions } from 'express';
+
+import { multerConfig } from './config/multer';
+import multer from 'multer';
+
 import { CountryController } from './controllers/CountryController';
 import { ManagerController } from './controllers/ManagerController';
 import { RoomController } from './controllers/RoomController';
 import { StudentController } from './controllers/StudentController';
 import {AuthenticateManager } from './controllers/AuthenticateManager'
 import { Auth } from './middlewares/authMiddleware';
-
+import {PostImage} from './controllers/ImagePostController'
 
 const router = Router();
 const authenticateManager = new AuthenticateManager()
 router.post('/session',authenticateManager.authenticate)
 
 
-
+ 
 
 
 
 const studentController = new StudentController()
 
 
-router.post("/students",studentController.create)
+router.post("/students",multer(multerConfig).single('file'),studentController.create)
 
 
 router.get("/students",studentController.show)
@@ -48,5 +52,21 @@ router.post("/manager",managerController.create)
 const auth = new Auth();
 router.get('/manager',auth.authMiddleware,managerController.index)
 
+/**************************************** */
 
+router.get('/foto', (request: Request, response: Response) => {
+    return response.json({ message: 'Hello Code83' })
+})
+
+/* router.post('/posts',multer(multerConfig).single('file'), (request: Request, response: Response) => {
+    
+    console.log(request.file)
+    
+    return response.json({ message: 'Imagem enviada' })
+}) */
+
+const imagePostController = new PostImage();
+
+router.post('/posts',multer(multerConfig).single('file'),imagePostController.create)
+router.get('/posts',imagePostController.show)
 export {router}
