c828f457430ba70008cda0577ca9112e7f34cf25
77-aguarosada <luisrosada2018@gmail.com>
2021-04-24 18:16:06 -0300

    update server.ts to use file static and morgan

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata"
 import express from "express";
 import cors from "cors"
+import path from 'path'
+import morgan from 'morgan'
 
 const app = express();
 import './database'
@@ -9,9 +11,15 @@ app.use(express.json());
 import { router } from "./routes";
 
 app.use(cors())
+
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev "));
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname,"..", "tmp", "uploads"))
+);
 app.use(router)
-
-
 app.listen(3333,()=>{
     console.log("server is runing");
 });
+
