bd0ba759c2d46613bb173905c65a4d3ae681de02
77-aguarosada <luisrosada2018@gmail.com>
2021-04-24 18:37:54 -0300

    updated file singount

--- web/src/pages/SingOut/index.js
+++ web/src/pages/SingOut/index.js
@@ -1,108 +1,85 @@
-import {useState} from 'react';
-import './styles.css'
-import out from "../../images/out.png"
+import React, { useState } from 'react';
+import './styles.css';
+import { toast } from 'react-toastify';
+import out from '../../images/out.png';
 
+import api from '../../services/api';
 
-import api from '../../services/api'
+export default function SingOut() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
+  async function handleSubmit() {
+    const data = { name: nome, email, password: senha };
 
-export default function SingOut(){
+    if (nome !== '' && email !== '' && senha !== '') {
+      const response = await api.post('/manager', data);
 
-    const [nome, setNome]= useState('');
-    const [email, setEmail]= useState('');
-    const [senha, setSenha]= useState('');
-
-   async function handleSubmit(){
-        const data = {name:nome,email:email,password:senha}
-       
-     if(nome !=='' && email !== ''&& senha !==''){
-
-        const response = await api.post('/manager',data)
-        
-        if(response.status!==400){
-            window.location.href='/home'
-        }
-        else{
-             alert('Error ao cadastar o usuario!')
-          }
-
-     } else{
-        alert('Error preencha os campos !')
-     }
-       
+      if (response.status !== 400) {
+        window.location.href = '/home';
+      } else {
+        toast.error('Error ao cadastar o usuario!');
+      }
+    } else {
+      toast.error('Error preencha os campos !');
     }
-     function goTologin(){
-        window.location.href='/'
-     }
-    return (
-        <div id="section">
-        
+  }
+  function goTologin() {
+    window.location.href = '/';
+  }
+  return (
+    <div id="section">
+      <div id="tel-sing">
+        <div id="tel-sing-container">
+          <h2>Cadastro.</h2>
+          <span>Digite seus dados para entrar e acessar o conteúdo.</span>
 
-        <div id="tel-sing">
-           <div id="tel-sing-container" >
-                <h2>Cadastro.</h2>
-                <span>Digite seus dados para entrar e acessar o conteúdo.</span>
-          
-         
-               <input
-                name="nome"
-                className="input"
-                type="text"
-                placeholder="Nome"
-               
-                value={nome}
-                onChange={e=> setNome(e.target.value)}
-                   
-                   />
-               <input 
-               name="email" 
-               className="input"
-               autoComplete="email" 
-               type="text" 
-               placeholder="E-mail"
-               
-               value={email}
-               onChange={e=>setEmail(e.target.value)}
-               />
-               <input 
-               name="senha"
-                className="input" 
-                type="password" 
-                required
-                autoComplete="senha"
-                placeholder="Senha"
-                
-                value={senha}
-                onChange={e=>setSenha(e.target.value)}
-                />
-               
-               <button 
-               className="cadastrar btn"
-               type="submit"
-               onClick={handleSubmit}
-               >Cadastrar</button>
-           </div>
+          <input
+            name="nome"
+            className="input"
+            type="text"
+            placeholder="Nome"
+            value={nome}
+            onChange={e => setNome(e.target.value)}
+          />
+          <input
+            name="email"
+            className="input"
+            autoComplete="email"
+            type="text"
+            placeholder="E-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            name="senha"
+            className="input"
+            type="password"
+            required
+            autoComplete="senha"
+            placeholder="Senha"
+            value={senha}
+            onChange={e => setSenha(e.target.value)}
+          />
 
-           <button 
-           className="cadastrar tbn"
-           onClick={goTologin}
-           >
-               Já tem uma conta? Faça login.
-               </button>
-
-
+          <button
+            className="cadastrar btn"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Cadastrar
+          </button>
         </div>
 
-        <div id="tm-img-sing">
-            <img id="tm-sing"src={out} alt=""/>
+        <button type="submit" className="cadastrar tbn" onClick={goTologin}>
+          Já tem uma conta? Faça login.
+        </button>
+      </div>
 
-        </div>
-
-
+      <div id="tm-img-sing">
+        <img id="tm-sing" src={out} alt="" />
+      </div>
     </div>
-
-
-
-    );
-
+  );
 }
