2d3e3f0dc53d029d7e2976a002cfd9da419f34ef
77-aguarosada <luisrosada2018@gmail.com>
2021-04-24 18:51:34 -0300

    Modified the router principal on backend and update

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,47 +1,35 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import {Route,Switch} from 'react-router-dom'
-import AllCountry from './pages/AllCountry';
-import AllRooms from './pages/AllRooms';
 import AllStudent from './pages/AllStudent';
-import CreateCountry from './pages/CreateCountry';
-import CreateRooms from './pages/CreatedRooms';
-import CreateStudent from './pages/CreateSudent';
 
+import CreateStudent from './pages/CreateSudent';
 
 import DeleteStudent from './pages/DeleteStudent';
 
 import EditStudent from './pages/EditStudent';
 import Home from './pages/Home';
 
-import SingIn from './pages/SingIn'
+import SingIn from './pages/SingIn';
 import SingOut from './pages/SingOut';
 
-function Routes(){
+import student from './pages/Estudenty/student';
+import Navbar from './components/Navbar/Navbar';
 
+function Routes() {
   return (
-         
     <Switch>
-       
-        <Route path="/" exact component={SingIn} />
-        <Route path="/cadastrar" exact component={SingOut} />
-        <Route path="/home" exact component={Home} />
-        <Route path="/student" exact component={CreateStudent} />
-        <Route path="/editstudent" exact component={EditStudent} />
-        <Route path="/allstudent" exact component={AllStudent} />
-        <Route path="/deletstudent" exact component={DeleteStudent} />
-        <Route path="/createrooms" exact component={CreateRooms} />
-        
-      
-        <Route path="/allrooms" exact component={AllRooms} />
-
-        <Route path="/allcountry" exact component={AllCountry} />
-        <Route path="/createcountry" exact component={CreateCountry} />
-      
+      <Route path="/" exact component={SingIn} />
+      <Route path="/cadastrar" exact component={SingOut} />
+      <Route path="/home" exact component={Home} />
+      <Route path="/student" exact component={CreateStudent} />
+      <Route path="/editstudent" exact component={EditStudent} />
+      <Route path="/allstudent" exact component={AllStudent} />
+      <Route path="/deletstudent" exact component={DeleteStudent} />
+      <Route path="/all" exact component={student} />
+      <Route path="/nav" exact component={Navbar} />
     </Switch>
-        
-
   );
-
 }
 
 export default Routes;
