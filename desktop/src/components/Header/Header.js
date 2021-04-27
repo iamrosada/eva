3c41594216bd1bdb5a6a32fa043bf987c4be8ad7
77-aguarosada <luisrosada2018@gmail.com>
2021-04-26 22:59:04 -0300

    created file for header desktop

---
+++ desktop/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MdBusiness } from 'react-icons/md';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header id="header-home-page">
+      <div>
+        <span id="das-home-page">evaBoard</span>
+      </div>
+      <div />
+
+      <div id="avatar-home-page">
+        <div id="icon-univer-page">
+          <MdBusiness size={20} />
+          <span className="avatar-name-univer">VSUET</span>
+        </div>
+        <span id="user-page">Luis Água Rosada</span>
+      </div>
+    </header>
+  );
+}
