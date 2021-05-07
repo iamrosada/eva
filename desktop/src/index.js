ddd10e4894defc59a6a06c26ca07f305f2ae45f3
77-aguarosada <luisrosada2018@gmail.com>
2021-05-07 17:56:39 -0300

    any change in index app

--- desktop/src/index.js
+++ desktop/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import { Router } from 'react-router-dom';
 import App from './App';
+// import history from './history';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Router history={history}> */}
     <App />
+    {/*  </Router> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
