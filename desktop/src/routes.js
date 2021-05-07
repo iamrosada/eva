f3658f64191c06ef4c6df86cc2cc8199acf37590
77-aguarosada <luisrosada2018@gmail.com>
2021-05-07 17:52:17 -0300

    updated router

--- desktop/src/routes.js
+++ desktop/src/routes.js
@@ -13,21 +13,17 @@ import Home from './pages/Home';
 import SingIn from './pages/SingIn';
 import SingOut from './pages/SingOut';
 
-import student from './pages/Estudenty/student';
-import Navbar from './components/Navbar/Navbar';
-
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SingIn} />
       <Route path="/cadastrar" exact component={SingOut} />
+
       <Route path="/home" exact component={Home} />
       <Route path="/student" exact component={CreateStudent} />
       <Route path="/editstudent" exact component={EditStudent} />
       <Route path="/allstudent" exact component={AllStudent} />
       <Route path="/deletstudent" exact component={DeleteStudent} />
-      <Route path="/all" exact component={student} />
-      <Route path="/nav" exact component={Navbar} />
     </Switch>
   );
 }
