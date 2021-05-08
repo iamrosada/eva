5f76d30736154c54b6a65b3a897bec692ef33e7c
77-aguarosada <luisrosada2018@gmail.com>
2021-05-08 21:35:48 +0300

    updated multer for work with S3 AWS ultimo commit depos do aws 8.05.2021

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,11 +1,11 @@
-
 import multer from 'multer';
-import path from 'path'
-import crypto from'crypto'
+import path from 'path';
+import crypto from'crypto';
+import aws from 'aws-sdk';
+import multerS3 from 'multer-s3';
 
-export const multerConfig = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local:multer.diskStorage({
     destination: (request, file, callback) => {
       callback(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
     },
@@ -14,12 +14,32 @@ export const multerConfig = {
         if (error) {
           callback(error, file.filename)
         }
+        /* const filename */file.key = `${hash.toString('hex')}-${file.originalname}`;
+        
+        callback(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'eva2',
+    contentType:multerS3.AUTO_CONTENT_TYPE,
+    acl:'public-read',
+    key:(request , file,callback )=>{
+      crypto.randomBytes(16, (error, hash) => {
+        if (error) {
+          callback(error, file.filename)
+        }
         const filename = `${hash.toString('hex')}-${file.originalname}`;
         
         callback(null, filename);
-      })
+      });
     }
   }),
+};
+export const multerConfig = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes['s3'],
   limits: {
     fileSize: 5 * 1024 * 1024 // 5MB
   },
