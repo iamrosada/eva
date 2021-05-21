4c6d1b7964478913772e1bfb3038683136d5aad5
77-aguarosada <luisrosada2018@gmail.com>
2021-05-21 15:12:47 +0300

    updated header.js

--- desktop/src/components/Header/Header.js
+++ desktop/src/components/Header/Header.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { MdBusiness } from 'react-icons/md';
 import './Header.css';
+import { useHistory } from 'react-router-dom';
 
 export default function Header() {
+  const history = useHistory();
+  const goTocadastro = () => {
+    // window.location.href = '/cadastrar';
+    history.push('/');
+  };
   return (
     <header id="header-home-page">
       <div>
         <span id="das-home-page">evaBoard</span>
       </div>
-      <div />
+      <div>
+        <h2 id="vsuet-name">
+          Воронежский государственный университет инженерных технологий
+        </h2>
+      </div>
 
       <div id="avatar-home-page">
         <div id="icon-univer-page">
           <MdBusiness size={20} />
-          <span className="avatar-name-univer">VSUET</span>
+          <span className="avatar-name-univer">ВГУИТ</span>
+
+          <button
+            type="submit"
+            className="tbnn"
+            onClick={e => {
+              goTocadastro(e);
+            }}
+          >
+            выйти
+          </button>
         </div>
-        <span id="user-page">Luis Água Rosada</span>
+
+        {/*  <span id="user-page">Luis Água Rosada</span> */}
       </div>
     </header>
   );
