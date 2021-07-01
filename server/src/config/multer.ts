4d819738215411de7f327d71e05abfb55e37078e
77-aguarosada <luisrosada2018@gmail.com>
2021-07-01 09:59:25 +0300

    updated file multer

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -1,60 +1,60 @@
-import multer from 'multer';
-import path from 'path';
-import crypto from'crypto';
-import aws from 'aws-sdk';
-import multerS3 from 'multer-s3';
+import { Response, Request, Express } from "express";
+import multer from "multer";
+import path from "path";
+import crypto from "crypto";
+import aws from "aws-sdk";
+import multerS3 from "multer-s3";
 
 const storageTypes = {
-  local:multer.diskStorage({
+  local: multer.diskStorage({
     destination: (request, file, callback) => {
-      callback(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
+      callback(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
     filename: (request, file, callback) => {
       crypto.randomBytes(16, (error, hash) => {
         if (error) {
-          callback(error, file.filename)
+          callback(error, file.filename);
         }
-        /* const filename */file.key = `${hash.toString('hex')}-${file.originalname}`;
-        
-        callback(null, file.key);
+        const filename = `${hash.toString("hex")}-${file.originalname}`;
+
+        callback(null, filename);
       });
-    }
+    },
   }),
   s3: multerS3({
     s3: new aws.S3(),
-    bucket: 'eva2',
-    contentType:multerS3.AUTO_CONTENT_TYPE,
-    acl:'public-read',
-    key:(request , file,callback )=>{
+    bucket: "eva2",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (request, file, callback) => {
       crypto.randomBytes(16, (error, hash) => {
         if (error) {
-          callback(error, file.filename)
+          callback(error, file.filename);
         }
-        const filename = `${hash.toString('hex')}-${file.originalname}`;
-        
+        const filename = `${hash.toString("hex")}-${file.originalname}`;
+
         callback(null, filename);
       });
-    }
+    },
   }),
 };
 export const multerConfig = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes['s3'],
+  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
+  storage: storageTypes["s3"],
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB
+    fileSize: 5 * 1024 * 1024, // 5MB
   },
-  fileFilter: (request, file, callback) => {
-    const formats = [
-      'image/jpeg',
-      'image/jpg',
-      'image/png',
-      'image/gif'
-    ];
+  fileFilter: (
+    request /* :Request */,
+    file /* :Express.Multer.File */,
+    callback /* :any */
+  ) => {
+    const formats = ["image/jpeg", "image/jpg", "image/png", "image/gif"];
 
     if (formats.includes(file.mimetype)) {
-      callback(null, true)
-    }  else {
-      callback(new Error('Format not accepted'))
+      callback(null, true);
+    } else {
+      callback(new Error("Format not accepted"));
     }
-  }
-} 
+  },
+};
