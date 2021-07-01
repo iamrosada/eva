1a47b83b571e29bb9e23d187c55fd5de4a3d8e16
77-aguarosada <luisrosada2018@gmail.com>
2021-07-01 10:01:13 +0300

    updated country controller

--- server/src/controllers/CountryController.ts
+++ server/src/controllers/CountryController.ts
@@ -1,49 +1,41 @@
-import { Response , Request} from "express"
+import { Response, Request } from "express";
 import { getCustomRepository } from "typeorm";
-import {  CountriesRepository } from "../repositories/CountriesRepository";
+import { CountriesRepository } from "../repositories/CountriesRepository";
 
-class CountryController{
+class CountryController {
+  async create(request: Request, response: Response) {
+    const { countryStudent } = request.body;
 
-    async create( request:Request, response:Response){
-        const { countryStudent} = request.body;
+    const countryRepository = getCustomRepository(CountriesRepository);
+    const countryUpperCase = countryStudent.toUpperCase();
+    // console.log(countryUpperCase)
+    //process.exit()
+    const countryAlreadyExists = await countryRepository.findOne({
+      countryStudent: countryUpperCase,
+    });
 
-        const countryRepository = getCustomRepository(CountriesRepository);
-        const countryUpperCase = countryStudent.toUpperCase();
-        // console.log(countryUpperCase)
-        //process.exit()
-        const countryAlreadyExists = await countryRepository.findOne({
-            countryStudent:countryUpperCase,
-        })
-
-        if(countryAlreadyExists){
-            return response.status(400).json({
-                error:"Country already exits",
-            })
-        
-        }
-
-        const CountryStudenty = countryRepository .create({
-            countryStudent:countryUpperCase,
-        })
-        
-        await countryRepository.save(CountryStudenty)
-
-        return response.send(CountryStudenty);
-
-
+    if (countryAlreadyExists) {
+      return response.status(400).json({
+        error: "Country already exits",
+      });
     }
 
-   
-    async show(request:Request, response:Response){
-     
-        const countryRepository = getCustomRepository(CountriesRepository);
+    const CountryStudenty = countryRepository.create({
+      countryStudent: countryUpperCase,
+    });
 
-        const allcountries = await countryRepository.find()
-             console.log(allcountries);
-        return response.json(allcountries);
+    await countryRepository.save(CountryStudenty);
 
-    }
- 
+    return response.send(CountryStudenty);
+  }
+
+  async show(request: Request, response: Response) {
+    const countryRepository = getCustomRepository(CountriesRepository);
+
+    const allcountries = await countryRepository.find();
+
+    return response.json(allcountries);
+  }
 }
 
-export {CountryController}
+export { CountryController };
