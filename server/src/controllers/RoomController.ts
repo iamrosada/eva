d1d594c9b03ab291349036c8943f7aff908633dc
77-aguarosada <luisrosada2018@gmail.com>
2021-07-01 10:03:00 +0300

    updated Room controller

--- server/src/controllers/RoomController.ts
+++ server/src/controllers/RoomController.ts
@@ -1,59 +1,50 @@
-import { Response , Request} from "express"
+import { Response, Request } from "express";
 import { getCustomRepository } from "typeorm";
 import { RoomsRepository } from "../repositories/RoomsRepository";
 
-class RoomController{
+class RoomController {
+  async create(request: Request, response: Response) {
+    const { numberofRoom } = request.body;
 
-    async create( request:Request, response:Response){
-        const {numberofRoom} = request.body;
+    const roomsRepository = getCustomRepository(RoomsRepository);
 
-        const roomsRepository = getCustomRepository(RoomsRepository);
-    
-        const stundAlreadyExists = await roomsRepository .findOne({
-            numberofRoom,
-        })
+    const roomAlreadyExists = await roomsRepository.findOne({
+      numberofRoom,
+    });
 
-        if(stundAlreadyExists){
-            return response.status(400).json({
-                error:"Room already exits",
-            })
-        
-        }
-
-        const Roomstudent = roomsRepository .create({
-            numberofRoom,
-        })
-        
-        await roomsRepository .save(Roomstudent)
-
-        return response.send(Roomstudent);
-
-
-    }
-    async show(request:Request, response:Response){
-     
-        const roomsRepository = getCustomRepository(RoomsRepository);
-
-        const allrooms = await roomsRepository.find()
-
-        return response.json(allrooms);
-
-    }
-    async update(request:Request, response:Response){
-        const roomsRepository = getCustomRepository(RoomsRepository);
-        const allrooms = await roomsRepository.findOne(request.params.id)
-        if (allrooms ) {
-            getCustomRepository(RoomsRepository).merge(allrooms , request.body);
-            const results = await getCustomRepository(RoomsRepository).save(allrooms);
-            return response.json(results);
-          }
-          
-          return response.json({error: 'Not Room found'});
-
-        
+    if (roomAlreadyExists) {
+      return response.status(400).json({
+        error: "Room already exits",
+      });
     }
 
+    const Roomstudent = roomsRepository.create({
+      numberofRoom,
+    });
 
+    await roomsRepository.save(Roomstudent);
+
+    return response.send(Roomstudent);
+  }
+  async show(request: Request, response: Response) {
+    const roomsRepository = getCustomRepository(RoomsRepository);
+
+    const allrooms = await roomsRepository.find();
+
+    return response.json(allrooms);
+  }
+  async update(request: Request, response: Response) {
+    const roomsRepository = getCustomRepository(RoomsRepository);
+    const allrooms = await roomsRepository.findOne(request.params.id);
+
+    if (allrooms) {
+      getCustomRepository(RoomsRepository).merge(allrooms, request.body);
+      const results = await getCustomRepository(RoomsRepository).save(allrooms);
+      return response.json(results);
+    }
+
+    return response.json({ error: "Not Room found" });
+  }
 }
 
-export {RoomController}
+export { RoomController };
