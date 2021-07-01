c16dc3d4a6aafa35f35a235c2a3d3ea5655e1e90
77-aguarosada <luisrosada2018@gmail.com>
2021-07-01 09:59:56 +0300

    updated authentication file

--- server/src/controllers/AuthenticateManager.ts
+++ server/src/controllers/AuthenticateManager.ts
@@ -1,43 +1,45 @@
-import { Response , Request} from "express"
+import { Response, Request } from "express";
 import { getCustomRepository } from "typeorm";
-import {compare, hash} from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import { compare, hash } from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { ManagersRepository } from "../repositories/ManagersRepository";
 
-class AuthenticateManager{
-    async authenticate(request:Request, response:Response){
-        const {email, password} = request.body;
+class AuthenticateManager {
+  async authenticate(request: Request, response: Response) {
+    const { email, password } = request.body;
 
-        const managersRepository = getCustomRepository(ManagersRepository);
+    const managersRepository = getCustomRepository(ManagersRepository);
 
-        const checkManegerExists = await managersRepository.findOne({
-            where:{email},
-        });
-        
-        if(!checkManegerExists){
-            return response.status(400).json({
-                error:"Incorrect email/password combination",
-            })
-        }
-        const passwordMatched = await  compare(password, checkManegerExists.password);
+    const checkManegerExists = await managersRepository.findOne({
+      where: { email },
+    });
 
-        if(!passwordMatched){
-            return response.status(400).json({
-                error:"Incorrect email/password combination",
-            })
-    
-        }
-        
-        const token = jwt.sign({id:checkManegerExists.id},'secret' ,{expiresIn:'1d'})
-        
-        delete checkManegerExists.password
-        return response.json({
-            checkManegerExists,
-            token,
-        });
+    if (!checkManegerExists) {
+      return response.status(400).json({
+        error: "Incorrect email/password combination",
+      });
+    }
+    const passwordMatched = await compare(
+      password,
+      checkManegerExists.password
+    );
 
+    if (!passwordMatched) {
+      return response.status(400).json({
+        error: "Incorrect email/password combination",
+      });
     }
 
+    const token = jwt.sign({ id: checkManegerExists.id }, "secret", {
+      expiresIn: "1d",
+    });
+
+    delete checkManegerExists.password;
+    return response.json({
+      checkManegerExists,
+      token,
+    });
+  }
 }
 
-export {AuthenticateManager}
+export { AuthenticateManager };
